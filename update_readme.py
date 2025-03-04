79addb40c2553d79176ba0d75e8ad429b206ded2
Tatze <144945036+T4tze@users.noreply.github.com>
2025-03-04 14:01:02 +0100

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -7,8 +7,9 @@ IMAGE_FOLDER = "Wallpaper"
 README_FILE = "README.md"
 
 # Header für die README.md
-readme_content = """# Here are my Wallpaper 
-U cant look at my Wallpapers here:
+readme_content = """# Looki ati mai wallis
+
+Here are my wallis:
 
 """
 
@@ -16,12 +17,15 @@ U cant look at my Wallpapers here:
 for root, _, files in os.walk(IMAGE_FOLDER):
     relative_path = os.path.relpath(root, IMAGE_FOLDER)
     if relative_path != ".":
-        readme_content += f"## {relative_path}\n\n"
+        readme_content += f"<details>\n  <summary><b>{relative_path}</b></summary>\n\n"
     
     for filename in sorted(files):
         if filename.lower().endswith((".png", ".jpg", ".jpeg", ".webp")):
             image_url = f"https://raw.githubusercontent.com/{USERNAME}/{REPO_NAME}/main/{root}/{filename}"
-            readme_content += f"![{filename}]({image_url})\n\n"
+            readme_content += f"  <img src=\"{image_url}\" width=\"300\">\n\n"
+    
+    if relative_path != ".":
+        readme_content += "</details>\n\n"
 
 # README.md speichern
 with open(README_FILE, "w", encoding="utf-8") as f:
